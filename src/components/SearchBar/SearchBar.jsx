6e2353bd11e0c77306b62ce7af86af36f19370f7
SrAbnerP <abner.sol.pereira@gmail.com>
2024-01-31 19:24:43 -0300

    creating the search function

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,9 +3,17 @@ import React, { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 
 import "./SearchBar.css";
+import fetchProducts from "../../api/fetchProducts";
 
 function SearchBar() {
   const [searchValue, setSearchValue] = useState("");
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+
+    const products = await fetchProducts(searchValue);
+    setSearchValue("");
+  };
 
   return (
     <form className="search-bar">
